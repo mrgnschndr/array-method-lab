bb6e8dba31d6a1dd757a017059f6765c38eb8229
mrgnschndr <mrgnschndr@gmail.com>
2024-09-26 23:20:34 -0600

    Complete Challenge 3

--- __tests__/arrayMethods.test.js
+++ __tests__/arrayMethods.test.js
@@ -1,7 +1,7 @@
 const {
     swapFirstAndLast,
     squareFirstIndex,
-    // getLongerArray,
+    getLongerArray,
     // arrayHasValue,
     // getLastIndexOf,
     // getNumberOfTimes,
@@ -37,13 +37,13 @@ describe('JavaScript Array Methods Tests', () => {
         expect(squareFirstIndex([true])).toBe('Invalid input');
     });
 
-    // // Test for getLongerArray
-    // test('getLongerArray should return the longer array', () => {
-    //     expect(getLongerArray([1, 2], [1, 2, 3])).toEqual([1, 2, 3]);
-    //     expect(getLongerArray([5, 4, 3], [1])).toEqual([5, 4, 3]);
-    //     expect(getLongerArray([1], [])).toEqual([1]);
-    //     expect(getLongerArray('not an array', [1])).toBe('Invalid input');
-    // });
+    // Test for getLongerArray
+    test('getLongerArray should return the longer array', () => {
+        expect(getLongerArray([1, 2], [1, 2, 3])).toEqual([1, 2, 3]);
+        expect(getLongerArray([5, 4, 3], [1])).toEqual([5, 4, 3]);
+        expect(getLongerArray([1], [])).toEqual([1]);
+        expect(getLongerArray('not an array', [1])).toBe('Invalid input');
+    });
 
     // // Test for arrayHasValue
     // test('arrayHasValue should return true if value exists in the array', () => {
